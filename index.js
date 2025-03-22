fb03894054a6b16bdc8dfcd05ea5f74ee3722e53
robertkutwa <robertkutwa0@gmail.com>
2025-03-22 13:57:25 +0300

    Stitching Together the Three Pillars (CodeGrade)

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
 /* This is a file inserted to keep the learn IDE browser happy */
 const testVar = {}
+const glyphStates = {
+    "♡": "♥",
+    "♥": "♡"
+  };
+  
+  const colorStates = {
+    "red": "",
+    "": "red"
+  };
+  
+  // STEP 1: Select all like glyphs (hearts)
+  const articleHearts = document.querySelectorAll(".like-glyph");
+  
+  function likeCallback(e) {
+    const heart = e.target;
+    mimicServerCall()
+      .then(function(serverMessage) {
+        alert("You notified the server!");
+        alert(serverMessage);
+        heart.innerText = glyphStates[heart.innerText];
+        heart.style.color = colorStates[heart.style.color];
+      })
+      .catch(function(error) {
+        alert("Something went wrong!");
+      });
+  }
+  
+  // STEP 3: Add event listeners to each heart
+  for (const glyph of articleHearts) {
+    glyph.addEventListener("click", likeCallback);
+  }
+  
+  //------------------------------------------------------------------------------
+  // Ignore after this point. Used only for demo purposes
+  //------------------------------------------------------------------------------
+  
+  function mimicServerCall() {
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        resolve("Pretend remote server notified of action!");
+      }, 300);
+    });
+  }
+  
